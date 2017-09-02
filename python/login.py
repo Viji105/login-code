38d1f9b047274bc231168fdf1d390ea162fd2cc6
Viji105 <31217216+Viji105@users.noreply.github.com>
2017-09-02 10:31:31 +0530

    login basic code
    this is a simple validation code i python.

---
+++ python/login.py
@@ -0,0 +1,43 @@
+# login
+import time
+
+print("please enter username")
+
+name = input()
+
+username = 'viji'
+
+if name == (username):
+    print("please wait")
+    time.sleep(1.5)
+    print('')
+    print("pls enter ur password")
+
+password = input()
+password1 = 'hello'
+if name != (username):
+    print("please wait")
+    time.sleep(1.5)
+    print('')
+    print("incorrect username")
+
+if password == (password1):
+    print("please wait")
+    time.sleep(1.5)
+    print('')
+    print("correct.login successful")
+    print("WELCOME")
+    exit
+
+
+
+
+
+
+
+
+
+
+
+
+
